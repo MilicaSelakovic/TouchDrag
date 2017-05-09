9ca9851e36e8ccf1ab108ac6609acff6d26b4f29
milica <milica1793@gmail.com>
2017-05-09 22:02:18 +0200

    added Triangle

---
+++ descretegeometrycalculations/src/main/java/com/example/descretegeometrycalculations/Triangle.java
@@ -0,0 +1,14 @@
+package com.example.descretegeometrycalculations;
+
+import java.util.Vector;
+
+/**
+ * Created by milica on 9.5.17..
+ */
+
+public class Triangle extends Polygon {
+
+    public Triangle(Vector<GeomPoint> points) {
+        super(points);
+    }
+}
